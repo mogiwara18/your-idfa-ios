8f60c89e6e39f310caafac107125ed11ae07c5d4
naoto0822 <n.h.in.m.h@gmail.com>
2017-02-18 22:35:35 +0900

    touch AppId class

---
+++ YourIDFA/Common/AppIdentifier.swift
@@ -0,0 +1,23 @@
+//
+//  AppIdentifier.swift
+//  YourIDFA
+//
+//  Created by naoto yamaguchi on 2017/02/18.
+//  Copyright © 2017年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+final class AppIdentifier {
+    
+    // MARK: - Public
+    
+    public static func appVersion() -> String {
+        guard let info = Bundle.main.infoDictionary,
+            let version = info["CFBundleShortVersionString"] as? String else {
+                return ""
+        }
+        
+        return version
+    }
+}
