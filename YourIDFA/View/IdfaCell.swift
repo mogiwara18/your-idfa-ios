c2bca532d5b8693022b048583f87a62b397803dd
naoto0822 <n.h.in.m.h@gmail.com>
2017-02-19 17:56:46 +0900

    modify idfa label

---
+++ YourIDFA/View/IdfaCell.swift
@@ -0,0 +1,29 @@
+//
+//  IdfaCell.swift
+//  YourIDFA
+//
+//  Created by naoto yamaguchi on 2017/02/19.
+//  Copyright © 2017年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+final class IdfaCell: UITableViewCell {
+    
+    // MARK: - Property
+    
+    @IBOutlet weak var idfaLabel: UILabel!
+    
+    // MARK: - LifeCycle
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
