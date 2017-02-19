44ec8374e9a6df88b239fb6e6ad990d66aeed64d
naoto0822 <n.h.in.m.h@gmail.com>
2017-02-19 20:34:40 +0900

    touch home view controller classes

---
+++ YourIDFA/ViewController/Home/HomeViewDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewDataSource.swift
+//  YourIDFA
+//
+//  Created by naoto yamaguchi on 2017/02/19.
+//  Copyright © 2017年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewDataSource: NSObject {
+
+}
