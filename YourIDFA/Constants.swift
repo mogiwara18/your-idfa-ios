cc866eefc39be0037f289f01f9b4597a7169662c
naoto0822 <n.h.in.m.h@gmail.com>
2017-02-19 15:56:04 +0900

    add constants

---
+++ YourIDFA/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  YourIDFA
+//
+//  Created by naoto yamaguchi on 2017/02/19.
+//  Copyright © 2017年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+public final class Constants {
+    public final class Share {
+        public static let MAIL_SUBJECT = ""
+    }
+}
