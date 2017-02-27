e29ffadb6c5a7a6f47f2d069d88aea27aafb3dc3
naoto0822 <n.h.in.m.h@gmail.com>
2017-02-27 23:21:22 +0900

    modify tracker

--- YourIDFA/Common/Tracker.swift
+++ YourIDFA/Common/Tracker.swift
@@ -8,6 +8,126 @@
 
 import UIKit
 
-class Tracker: NSObject {
-
+public final class Tracker {
+    
+    // MARK: - Singleton
+    
+    public static let shared = Tracker()
+    
+    // MARK: - Public
+    
+    public func sendAllEvent() {
+        self.sendStartSessionEvent()
+        self.sendViewProductEvent()
+        self.sendAddCartEvent()
+        self.sendViewBasketEvent()
+        self.sendCheckOutEvent()
+        self.sendPurchaseEvent()
+        self.sendPaymentInfoEvent()
+        self.sendAddFavoriteEvent()
+        self.sendSearchEvent()
+        self.sendShareEvent()
+        self.sendReviewEvent()
+        self.sendSignupEvent()
+        self.sendLoginEvent()
+        self.sendInviteEvent()
+        self.sendStartLevelEvent()
+        self.sendEndLevelEvent()
+        self.sendTutorialEvent()
+        self.sendDeepLinkEvent()
+        self.sendCustom1Event()
+        self.sendCustom2Event()
+        self.sendCustom3Event()
+        self.sendReservationEvent()
+    }
+    
+    // MARK: - Private
+    
+    private func sendStartSessionEvent() {
+        
+    }
+    
+    private func sendViewProductEvent() {
+        
+    }
+    
+    private func sendAddCartEvent() {
+        
+    }
+    
+    private func sendViewBasketEvent() {
+        
+    }
+    
+    private func sendCheckOutEvent() {
+        
+    }
+    
+    private func sendPurchaseEvent() {
+        
+    }
+    
+    private func sendPaymentInfoEvent() {
+        
+    }
+    
+    private func sendAddFavoriteEvent() {
+        
+    }
+    
+    private func sendSearchEvent() {
+        
+    }
+    
+    private func sendShareEvent() {
+        
+    }
+    
+    private func sendReviewEvent() {
+        
+    }
+    
+    private func sendSignupEvent() {
+        
+    }
+    
+    private func sendLoginEvent() {
+        
+    }
+    
+    private func sendInviteEvent() {
+        
+    }
+    
+    private func sendStartLevelEvent() {
+        
+    }
+    
+    private func sendEndLevelEvent() {
+        
+    }
+    
+    private func sendTutorialEvent() {
+        
+    }
+    
+    private func sendDeepLinkEvent() {
+        
+    }
+    
+    private func sendCustom1Event() {
+        
+    }
+    
+    private func sendCustom2Event() {
+        
+    }
+    
+    private func sendCustom3Event() {
+        
+    }
+    
+    private func sendReservationEvent() {
+        
+    }
 }
