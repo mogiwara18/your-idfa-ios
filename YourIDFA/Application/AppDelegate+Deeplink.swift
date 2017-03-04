2cd5047087bb5b5c4865a21d660072fece3e6fed
naoto0822 <n.h.in.m.h@gmail.com>
2017-03-04 19:20:07 +0900

    add new file

---
+++ YourIDFA/Application/AppDelegate+Deeplink.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+Deeplink.swift
+//  YourIDFA
+//
+//  Created by naoto yamaguchi on 2017/03/04.
+//  Copyright © 2017年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class AppDelegate_Deeplink: NSObject {
+
+}
