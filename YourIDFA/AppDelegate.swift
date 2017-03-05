50719226f97b21cd2b29745cfa5b6c8265042e6b
naoto0822 <n.h.in.m.h@gmail.com>
2017-03-05 17:06:35 +0900

    display log when launch app

--- YourIDFA/AppDelegate.swift
+++ YourIDFA/AppDelegate.swift
@@ -18,6 +18,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        let bundleID = AppIdentifier.bundleID()
+        
+        #if DEBUG
+            print("************************************************")
+            print("* Debug Build")
+            print("* bundle id: \(bundleID)")
+            print("************************************************")
+        #else
+            print("************************************************")
+            print("* Release Build")
+            print("* bundle id: \(bundleID)")
+            print("************************************************")
+        #endif
+        
         // fabric
         Fabric.with([Crashlytics.self])
         
